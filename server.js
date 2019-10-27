8850b4a872c77b6e5f4f4242b670ffdaa3010191
ivancab <ivo.cabrera29@gmail.com>
2019-10-27 15:36:28 -0300

    fix app

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the contact keeper api' })
-);
-
 // Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
